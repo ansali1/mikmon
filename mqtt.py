75a98934303c90e79bfa5a6197ce3806dc52e5e1
ansali1 <90696271+ansali1@users.noreply.github.com>
2021-09-14 16:29:41 +0300

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -9,7 +9,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("get/from/0089227f")
+    client.subscribe("get/from/0089227fmik")
  
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
